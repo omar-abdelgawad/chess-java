029313217774ab110e960b16c313dbc152ed493b
Khaled Anis <khaledanis@Khaleds-MacBook-Air.local>
2024-01-08 11:59:32 +0200

    final

--- src/ChessGame.java
+++ src/ChessGame.java
@@ -3,14 +3,13 @@ import user_interface.StartFrame;
 
 public class ChessGame {
     private ChessFrame chessFrame;
-    private StartFrame startFrame;
 
     public static void main(String[] args) throws Exception {
-        ChessGame game = new ChessGame();
+        new ChessGame();
     }
 
     ChessGame() {
         chessFrame = new ChessFrame();
-        startFrame = new StartFrame(chessFrame);
+        new StartFrame(chessFrame);
     }
 }
